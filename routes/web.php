33776e21525710e08e5668a9c0c0cebe1de3966f
Miguel Ángel García <magarcial.dev@outlook.es>
2025-09-22 17:40:14 +0200

    Create categories API

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\CategoriaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('api')->group(function () {
+    Route::apiResource('/categorias', CategoriaController::class); // /api/categorias URL
 });
